66f12d2d0fb653760b4cb42d326fdc070795be13
Rayon-Richter Malou <rayon.richter.malou@gmail.com>
2023-09-29 23:09:20 +0200

    TME2 question 3&4

--- TME2/main.cpp
+++ TME2/main.cpp
@@ -3,12 +3,12 @@
 #include <regex>
 #include <chrono>
 
-bool contains(const std::vector<std::string>& v, const std::string& s){
+int* contains(std::vector<std::pair<std::string, int>>& v, const std::string& s){
   for (auto i = v.begin(); i != v.end(); i++) {
-    if ((*i) == s)
-      return true;
+    if ((*i).first == s)
+      return &((*i).second);
   }
-  return false;
+  return nullptr;
 }
 
 int main () {
@@ -20,7 +20,7 @@ int main () {
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
 
-  std::vector mots_presents = std::vector<std::string>();
+  std::vector mots_presents = std::vector<std::pair<std::string, int>>();
 
 	size_t nombre_lu = 0;
 	// prochain mot lu
@@ -32,16 +32,22 @@ int main () {
 		word = regex_replace ( word, re, "");
 		// passe en lowercase
 		transform(word.begin(),word.end(),word.begin(),::tolower);
-
+    if (word.empty())
+      continue;
 		// word est maintenant "tout propre"
 		if (nombre_lu % 100 == 0)
 			// on affiche un mot "propre" sur 100
 			cout << nombre_lu << ": "<< word << endl;
 		nombre_lu++;
-    if (contains(mots_presents, word) == false){
-      mots_presents.push_back(word);
+    int* res = contains(mots_presents, word);
+    if (res == nullptr){
+      mots_presents.push_back(make_pair(word, 1));
+    }
+    else {
+      (*res)++;
     }
 	}
+
 	input.close();
 
 	cout << "Finished Parsing War and Peace" << endl;
@@ -53,6 +59,22 @@ int main () {
 
     cout << "Found a total of " << nombre_lu << " words." << endl;
     cout << "Found a total of " << mots_presents.size() << " differents words." << std::endl;
+
+    auto tab = std::vector<std::string>();
+    tab.push_back("toto");
+    tab.push_back("war");
+    tab.push_back("peace");
+    for (auto i = tab.begin(); i != tab.end(); i++) {
+      int* res = contains(mots_presents, *i); 
+      int r;
+      if (res == nullptr){
+        r = 0;
+      }
+      else {
+        r = *res;
+      }
+      cout << "Il y a " << r << " fois le mot " << *i << " dans le livre." << endl; 
+    }
     return 0;
 }
 
